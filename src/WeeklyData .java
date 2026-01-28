3ff92e98c94dd52c1494ee611fc5cc2210d40c1d
elenak07 <ekoleva73@gmail.com>
2026-01-28 06:48:31 +0000

    final

--- src/WeeklyData .java
+++ src/WeeklyData .java
@@ -2,6 +2,8 @@
  * The WeeklyData class stores and analyzes a week’s worth of numeric data.
  * This could represent steps taken, hours of sleep, money spent, screen time,
  * or any other measurable daily value.
+ * It will store the number of minutes read each day for a week (7 days)
+ * and provide methods to calculate total, average, maximum, and minimum values.
  */
 public class WeeklyData {
 
@@ -11,6 +13,10 @@ public class WeeklyData {
     // TODO: Declare a private array to store the week’s data
     //       Choose an appropriate type (double[] or int[])
     //       Create other instance variables as necessary
+        private int[] data;
+        private int days;
+        private int minutesPerDay;
+        private int totalMinutes;
     
 
 
@@ -28,6 +34,12 @@ public class WeeklyData {
         // TODO: Create a new array with the same length as input
         // TODO: Copy each value from input into the internal data array
         // NOTE: Do NOT do this.data = input; (that would create aliasing)
+
+        this.data = new int[input.length];
+        for (int i = 0; i < input.length; i++) {
+            this.data[i] = (int) input[i];
+        }
+
     }
 
 
@@ -43,7 +55,13 @@ public class WeeklyData {
         // TODO: Create a variable to store the running total
         // TODO: Use a loop to add each value in the array to the total
         // TODO: Return the total
-        return 0.0; // replace with your calculated total
+        //return 0.0; // replace with your calculated total
+
+        int total = 0;
+        for (int i = 0; i < data.length; i++) {
+            total += data[i];
+        }
+        return total;
     }
 
 
@@ -61,6 +79,11 @@ public class WeeklyData {
         // TODO: Otherwise, divide the total by the number of elements
         // Hint: You may call getTotal()
         return 0.0; // replace with your calculated average
+        if (data.length == 0){
+            return 0.0;
+        } else {
+            return getTotal() / data.length;
+        }
     }
 
 
@@ -77,6 +100,14 @@ public class WeeklyData {
         // TODO: Loop through the rest of the array and update max as needed
         // TODO: Return the maximum value found
         return 0.0; // replace with the maximum value
+
+        double max = data[0];
+        for (int i = 1; i < data.length; i++){
+            if (data[i] > max){
+                max = data[i];
+            }
+            return max;
+        }
     }
 
 
@@ -93,6 +124,14 @@ public class WeeklyData {
         // TODO: Loop through the rest of the array and update min as needed
         // TODO: Return the minimum value found
         return 0.0; // replace with the minimum value
+        double min = data[0];
+        for (int i = 1; i < data.length; i++){
+            if (data[i] < min){
+                min = data[i];
+            
+            }
+            return min;
+        }
     }
 
 
@@ -116,5 +155,11 @@ public class WeeklyData {
         // TODO: Append each value with a day label (Day 1, Day 2, etc.)
         // TODO: Return the completed String
         return ""; // replace with your formatted output
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < data.length; i++){
+            sb.append("Day " + (i + 1) + ": " + data[i] + "\n");
+        }
+        return sb.toString();
     }
 }
